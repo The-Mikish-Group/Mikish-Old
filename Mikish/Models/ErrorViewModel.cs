d399a9eda2c59fc034e6806cf7277b6486f23e17
The Mikish Group <MikishVaughn@users.noreply.github.com>
2023-01-09 15:51:15 -0600

    Add project files.

---
+++ Mikish/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mikish.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
